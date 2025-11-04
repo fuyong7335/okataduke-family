06d435fe46f1f68dca47963a361b7065009f8a3b
fuyong7335 <yukie353yukie@gmail.com>
2025-11-04 13:13:57 +0900

    Add files via upload

---
+++ okataduke-familymain_family.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.title("👪 散らかされタイプのあなたへ")
+st.write("💡 家族のモノに振り回されるあなたに、ストレスを減らす整理術をご紹介します。")
+st.markdown("▼アドバイスの続きは、無料メール講座でお届けしています👇")
+st.markdown("[📩 メールで受け取る（無料）](https://www.reservestock.jp/subscribe/221907)")
+
+
